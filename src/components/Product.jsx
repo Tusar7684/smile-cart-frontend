4de5cb304b410eefc7ba77abb25d09408e0670b2
Tusar7684 <b421065@iiit-bh.ac.in>
2024-08-06 11:48:56 +0530

    Created product API connector file

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,8 @@
 // import { Typography } from "neetoui";
 import { useState, useEffect } from "react";
 
-import axios from "axios";
+//import axios from "axios";
+import productsApi from "apis/products";
 import { Typography, Spinner } from "neetoui";
 import { append, isNotNil } from "ramda";
 
@@ -26,9 +27,10 @@ const Product = () => {
 
   const fetchProduct = async () => {
     try {
-      const response = await axios.get(
-        "https://smile-cart-backend-staging.neetodeployapp.com/products/infinix-inbook-2"
-      );
+      // const response = await axios.get(
+      //   "https://smile-cart-backend-staging.neetodeployapp.com/products/infinix-inbook-2"
+      // );
+      const response = await productsApi.show();
       setProduct(response.data);
     } catch (error) {
       console.log("An error occurred:", error);
